9c4f91f9af5f9aabed1d96b2c951a8bb8155190b
Ronny Omnier Gonzalez Vargas <yordemblo@gmail.com>
2024-10-04 21:25:32 +0200

    Corrige error del ejercicio del reto 5

--- listado/Reto #05 - Agua para Programadores/javascript/ronnyg2121.js
+++ listado/Reto #05 - Agua para Programadores/javascript/ronnyg2121.js
@@ -20,9 +20,10 @@ const main = (cantidad, botella1, botella2) => {
     let costo1 = botella1 * cantidad;
     let costo2 = botella2 * cantidad;
 
-    if (cantidad % 2 !== 0) {
+    if (cantidad % 2 == 0) {
         console.info(`El precio mínimo para comprar ${cantidad} litros de agua es: ${costo1}`);
     } else {
+
         console.info(`El precio mínimo para comprar ${cantidad} litros de agua es: ${costo2}`);
     }
     return;
